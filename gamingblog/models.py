fa5e58e85d5bdadde83dc14210c7fe1f655f3515
sahil4430 <panwarsahil204@gmail.com>
2024-07-22 23:41:51 +0530

    models implementation

--- gamingblog/models.py
+++ gamingblog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.\
+class BlogDetail(models.Model):
+    Blog_id = models.AutoField( primary_key=True)
+    Blog_Title = models.CharField(max_length=50)
+    Blog_Desc= models.CharField(max_length=400)
+    Blog_Date= models.DateTimeField()
+    class Meta:
+        db_table="BlogTable"
+
+        def __str__(self):
+            return self.blog_title
+
+
+
